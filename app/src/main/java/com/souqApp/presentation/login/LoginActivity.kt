6415c6833af867d8af307e6ce7adadaf03cfb508
hamza-helow <hamzihelow3@gmail.com>
2022-01-07 01:57:54 +0200

    -

--- app/src/main/java/com/souqApp/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/souqApp/presentation/login/LoginActivity.kt
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import com.souqApp.databinding.ActivityLoginBinding
+import com.souqApp.infra.utils.SharedPrefs
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
     val viewModel: LoginViewModel by viewModels()
+
+    @Inject
+    lateinit var sharedPrefs: SharedPrefs
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/souqApp/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/souqApp/presentation/login/LoginViewModel.kt
@@ -1,11 +1,70 @@
 package com.souqApp.presentation.login
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.souqApp.data.common.remote.dto.UserResponse
+import com.souqApp.data.common.utlis.WrappedResponse
+import com.souqApp.data.login.remote.dto.LoginRequest
+import com.souqApp.domain.common.BaseResult
+import com.souqApp.domain.common.entity.UserEntity
 import com.souqApp.domain.login.LoginUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(private val loginUseCase: LoginUseCase) : ViewModel() {
+    private val state = MutableStateFlow<LoginActivityState>(LoginActivityState.Init)
+    val mState: StateFlow<LoginActivityState> get() = state
 
+    private fun setLoading() {
+        state.value = LoginActivityState.IsLoading(true)
+    }
+
+    private fun hideLoading() {
+        state.value = LoginActivityState.IsLoading(false)
+    }
+
+    private fun showToast(message: String) {
+        state.value = LoginActivityState.ShowToast(message)
+    }
+
+
+    private fun successLogin(loginEntity: UserEntity) {
+        state.value = LoginActivityState.SuccessLogin(loginEntity)
+    }
+
+    private fun errorLogin(rawResponse: WrappedResponse<UserResponse>) {
+        state.value = LoginActivityState.ErrorLogin(rawResponse)
+    }
+
+
+    fun login(loginRequest: LoginRequest) {
+        viewModelScope.launch {
+            loginUseCase.invoke(loginRequest).onStart {
+                setLoading()
+            }.catch { error ->
+                hideLoading()
+                showToast(error.stackTraceToString())
+            }.collect { result ->
+                hideLoading()
+                when (result) {
+                    is BaseResult.Success -> successLogin(result.data)
+                    is BaseResult.Errors -> errorLogin(result.error)
+                }
+            }
+
+        }
+
+    }
+
+}
+
+sealed class LoginActivityState {
+    object Init : LoginActivityState()
+    data class IsLoading(val isLoading: Boolean) : LoginActivityState()
+    data class ShowToast(val message: String) : LoginActivityState()
+    data class SuccessLogin(val loginEntity: UserEntity) : LoginActivityState()
+    data class ErrorLogin(val rawResponse: WrappedResponse<UserResponse>) : LoginActivityState()
 }
